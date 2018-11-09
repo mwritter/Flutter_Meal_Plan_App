c239a9a278132eba3d41c4ce9ef4524210ae6669
Matthew Ritter <mritter2283@gmail.com>
2018-11-09 11:27:07 -0500

    Created Starting point for the Meal Plan flutter app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './pages/login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meal Plan',
+      routes: {
+        '/': (BuildContext context) => LoginPage(),
+      },
+    );
+  }
+}
