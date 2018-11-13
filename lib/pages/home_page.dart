e1d553c180690ca41d0a9fd2dae7de7cec33b0aa
Matthew Ritter <mritter2283@gmail.com>
2018-11-13 08:42:53 -0500

    First try on pulling data from Firestore, works but not fully correct yet

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatelessWidget {
+  Widget _buildMealList(BuildContext context) {
+    return StreamBuilder(
+      stream: Firestore.instance.collection('users').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Container(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Loading Please Wait"),
+                  CircularProgressIndicator(),
+                ],
+              ),
+            ),
+          );
+        }
+        return Column(
+          children: <Widget>[
+            Text(snapshot.data.documents[0]['name']),
+            Text(snapshot.data.documents[0]['email']),
+            Image(
+              image: NetworkImage(snapshot.data.documents[0]['image']),
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,6 +42,7 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text("HomePage"),
+            _buildMealList(context),
             RaisedButton(
               onPressed: () {
                 FirebaseAuth.instance.signOut().then((value) {
