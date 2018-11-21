80dd90eb5bb1546736a019db37fcecd67159ac45
Matthew Ritter <mritter2283@gmail.com>
2018-11-21 14:07:53 -0500

    Added reduced list to shopping list, still need to implement the count correctly though

--- lib/pages/shopping_list.dart
+++ lib/pages/shopping_list.dart
@@ -6,17 +6,38 @@ class ShoppingList extends StatelessWidget {
 
   ShoppingList(this.list);
 
+  List<String> reduceList() {
+    List<String> reduced = [];
+    for (String item in list) {
+      if (!reduced.contains(item)) {
+        reduced.add(item);
+      }
+    }
+    return reduced;
+  }
+
+  int getCount(String str) {
+    int count = 0;
+    for (String item in list) {
+      if (item == str) {
+        count++;
+      }
+    }
+    return count;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ListView.builder(
-        itemCount: list.length,
+        itemCount: reduceList().length,
         itemBuilder: (BuildContext context, int index) {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: ListTile(
+              leading: Text(getCount(list[index]).toString()),
               contentPadding: EdgeInsets.all(1.0),
-              title: Text(list[index]),
+              title: Text(reduceList()[index]),
             ),
           );
         },
